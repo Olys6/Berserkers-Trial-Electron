853594bb0a59560004b7523ee2d759a0d7d66262
Olys6 <oliver@skjevesland.com>
2022-05-10 13:02:44 +0200

    new

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
 const { app, BrowserWindow } = require('electron')
+// const electronInstaller = require('electron-winstaller');
+
+// try {
+//     await electronInstaller.createWindowsInstaller({
+//         appDirectory: '/tmp/build/my-app-64',
+//         outputDirectory: '/tmp/build/installer64',
+//         authors: 'My App Inc.',
+//         exe: 'Berserkers-Trial.exe'
+//     });
+//     console.log('It worked!');
+// } catch (e) {
+//     console.log(`No dice: ${e.message}`);
+// }
 
 const createWindow = () => {
     const win = new BrowserWindow({
